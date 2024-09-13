fafeb151f259959228d537c87b486bc6ab578e3a
Gollapalli Yakesh <yakesh114@gmail.com>
2024-09-13 19:29:04 +0530

    first commit

---
+++ arithematic.js
@@ -0,0 +1,40 @@
+const a=10;
+const b=20;
+const c=a+b;
+console.log(c)
+
+
+const a1=10;
+const b1=20;
+const c1=a-b;
+console.log(c1)
+
+const a2=10;
+const b2=20;
+const c2=a*b;
+console.log(c2)
+
+const a3=10;
+const b3=20;
+const c3=a/b;
+console.log(c3)
+
+const a4=10;
+const b4=20;
+const c4=a%b;
+console.log(c4)
+
+const a5=10;
+const b5=20;
+const c5=a**b;
+console.log(c5)
+
+let x=10;
+x++;
+let y=x;
+console.log(x)
+
+let p=10;
+p--;
+let q=p;
+console.log(p)
---
+++ assignment.js
@@ -0,0 +1,27 @@
+let s=10;
+s =5;
+console.log(s)
+
+let x=10;
+x +=5;
+console.log(x)
+
+let a=10;
+a -=5;
+console.log(a)
+
+let p=10;
+p *=5;
+console.log(p)
+
+let m=10;
+m /=5;
+console.log(m)
+
+let c=10;
+c %=5;
+console.log(c)
+
+let y=10;
+y **=5;
+console.log(y)
